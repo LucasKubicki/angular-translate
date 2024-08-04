397836ba23653a9a0d771325fcfb12628652f006
Lucas <kubicki.lucas@gmail.com>
2024-08-04 16:18:41 +0200

    add example in html + generate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-translate';
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  logo = '${this.baseUrl}/angular.svg';
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  }
+  male() {
+    this.gender = 'male';
+  }
+  female() {
+    this.gender = 'female';
+  }
+  other() {
+    this.gender = 'other';
+  }
 }
